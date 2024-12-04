883306917fc39991b2bd13b5931e4ee370a81e24
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-04 19:49:52 +0200

    fixed template

--- main.cpp
+++ main.cpp
@@ -6,25 +6,21 @@ int main() {
 
 
 
-	int* size_m = new int;
-	int* A = new int[*size_m] {};
-	int* size_n = new int;
-	int* B = new int[*size_n] {};
+	int size_m, size_n;
+	cout << "M: ";
+	cin >> size_m;
+	cout << "N: ";
+	cin >> size_n;
+
+	int* A = new int[size_m] {};
+	int* B = new int[size_n] {};
 
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<> dis(1, 100);
 
-	cout << "M: ";
-	cin >> *size_m;
-	cout << "N: ";
-	cin >> *size_n;
 
-
-
-	delete size_m;
 	delete[] A;
-	delete size_n;
 	delete[] B;
 
 	return 0;
