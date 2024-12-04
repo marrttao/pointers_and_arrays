58af6a79192ca257d8093cb8ab78f30c9945c68a
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-04 19:56:24 +0200

    fixed again

--- main.cpp
+++ main.cpp
@@ -6,20 +6,22 @@ int main() {
 
 
 
-	int size_m, size_n;
+	int* size_m = new int;
+	int* size_n = new int;
 	cout << "M: ";
-	cin >> size_m;
+	cin >> *size_m;
 	cout << "N: ";
-	cin >> size_n;
+	cin >> *size_n;
 
-	int* A = new int[size_m] {};
-	int* B = new int[size_n] {};
+	int* A = new int[*size_m] {};
+	int* B = new int[*size_n] {};
 
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<> dis(1, 100);
 
-
+	delete size_m;
+	delete size_n;
 	delete[] A;
 	delete[] B;
 
