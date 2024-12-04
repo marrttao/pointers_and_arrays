7e4d1fa3a9a8ca0e70f7eb48359cfdeb84dc1ac9
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-04 20:47:01 +0200

    fundament of task 3

--- main.cpp
+++ main.cpp
@@ -3,27 +3,31 @@
 using namespace std;
 
 int main() {
+    int* size_m = new int;
+    cout << "M: ";
+    cin >> *size_m;
+
+    int* A = new int[*size_m] {};
+
+    cout << "Enter elements for array A: ";
+    for (int i = 0; i < *size_m; i++) {
+        cin >> A[i];
+    }
+	for (int i = 0; i < *size_m; i++) {
+		cout << A[i] << " ";
+	}
+
+    cout << endl << "|| odd numbers - 1  ||";
+	cout << endl << "|| even numbers - 2 ||";
+	cout << endl << "|| Your choice: ";
+	int* choice = new int;
+	cin >> *choice;
 
 
 
-	int* size_m = new int;
-	int* size_n = new int;
-	cout << "M: ";
-	cin >> *size_m;
-	cout << "N: ";
-	cin >> *size_n;
 
-	int* A = new int[*size_m] {};
-	int* B = new int[*size_n] {};
+    delete size_m;
+    delete[] A;
 
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<> dis(1, 100);
-
-	delete size_m;
-	delete size_n;
-	delete[] A;
-	delete[] B;
-
-	return 0;
+    return 0;
 }
