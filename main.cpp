680f3faffa26c01133a40daeae4877ae92901976
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-04 20:24:49 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -3,27 +3,64 @@
 using namespace std;
 
 int main() {
+    int* size_m = new int;
+    int* size_n = new int;
+    cout << "M: ";
+    cin >> *size_m;
+    cout << "N: ";
+    cin >> *size_n;
 
+    int* A = new int[*size_m] {};
+    int* B = new int[*size_n] {};
 
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 2);
 
-	int* size_m = new int;
-	int* size_n = new int;
-	cout << "M: ";
-	cin >> *size_m;
-	cout << "N: ";
-	cin >> *size_n;
+    for (int i = 0; i < *size_m; i++) {
+        A[i] = dis(gen);
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < *size_n; i++) {
+        B[i] = dis(gen);
+        cout << B[i] << " ";
+    }
+    cout << endl;
 
-	int* A = new int[*size_m] {};
-	int* B = new int[*size_n] {};
+    int* temp = new int[*size_m + *size_n] {};
+    int temp_size = 0;
 
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<> dis(1, 100);
+    for (int i = 0; i < *size_m; i++) {
+        bool already_added = false;
+        for (int k = 0; k < temp_size; k++) {
+            if (temp[k] == A[i]) {
+                already_added = true;
+                break;
+            }
+        }
+        if (already_added) continue;
 
-	delete size_m;
-	delete size_n;
-	delete[] A;
-	delete[] B;
+        for (int j = 0; j < *size_n; j++) {
+            if (A[i] == B[j]) {
+                temp[temp_size] = A[i];
+                temp_size++;
+                break;
+            }
+        }
+    }
 
-	return 0;
+    cout << "Common elements: ";
+    for (int i = 0; i < temp_size; i++) {
+        cout << temp[i] << " ";
+    }
+    cout << endl;
+
+    delete size_m;
+    delete size_n;
+    delete[] A;
+    delete[] B;
+    delete[] temp;
+
+    return 0;
 }
