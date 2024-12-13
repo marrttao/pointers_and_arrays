ba2e5c0b7451963875c453ecd250bdf7273ad0a7
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-14 00:06:28 +0200

    completed task 1

--- main.cpp
+++ main.cpp
@@ -18,7 +18,52 @@ int main() {
 
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<> dis(1, 100);
+	uniform_int_distribution<> dis(1, 10);
+
+	cout << "Array A: " << endl;
+
+	for (int i = 0; i < *size_m; i++) {
+		A[i] = dis(gen);
+		cout << A[i] << " ";
+	}
+
+	cout << endl << "Array B: " << endl;
+
+	for (int i = 0; i < *size_n; i++) {
+		B[i] = dis(gen);
+		cout << B[i] << " ";
+	}
+
+	cout << endl;
+
+	int* size_c = new int;
+	*size_c = *size_m ;
+	int* C = new int[*size_c] {};
+	
+	int* index = new int;
+	*index = 0;
+	for (int i = 0; i < *size_m; i++) {
+		bool is_repeated = false;
+		for (int j = 0; j < *size_n; j++) {
+			if (A[i] == B[j]) {
+				is_repeated = true;
+				break;
+			}
+		}
+		if (!is_repeated) {
+			C[*index] = A[i];
+			(*index)++;
+		}
+	}
+
+	*size_c = *index;
+	cout << "Array C: " << endl;
+	for (int i = 0; i < *size_c; i++) {
+		cout << C[i] << " ";
+	}
+	
+
+
 
 	delete size_m;
 	delete size_n;
